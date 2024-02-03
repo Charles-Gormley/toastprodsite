64eaeccafdbbee6c631e4aa48b4275e939375ede
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2024-02-03 17:44:12 -0500

    Update Hero component in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import Hero from "../components/Hero";
 export default function Home() {
   return (
     <div className="min-h-screen bg-white flex flex-col dark:bg-gray-900">
-      <Hero title="THE FUTURE OF NEWS!" subtitle="BRB" />
+      <Hero
+        title="The Future of News is Here"
+        subtitle="Experience AI Curated Podcasts at your fingertips. Hear only the stories YOU want to hear, without the bias. With sources from thousands of articles, you can learn more in just one click."
+      />
     </div>
   );
 }
