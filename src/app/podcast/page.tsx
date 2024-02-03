5e24dd3dec1f3db0532ca26c1ab56009119c90c6
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2024-02-03 16:58:02 -0500

    Add podcast page component

---
+++ src/app/podcast/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+    return (
+      <div className="min-h-screen bg-white flex flex-col dark:bg-gray-900">
+          <h1>Podcast PAGE</h1>
+      </div>
+    );
+  }
+  
