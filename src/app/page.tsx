8dbd49d235b927ab276b7212b444d5464abaa69e
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2024-02-03 16:57:50 -0500

    Remove Navbar component from Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Image from "next/image";
 import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white flex flex-col dark:bg-gray-900">
-      <Navbar/>
       <Hero title="THE FUTURE OF NEWS!" subtitle="BRB" />
     </div>
   );
