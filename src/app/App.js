3af64ae9751d26ca43a306c4d7cafb5af0869531
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2024-02-02 22:09:18 -0500

    Refactor App.js to include Navbar component

--- src/app/App.js
+++ src/app/App.js
@@ -1,31 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Home'; 
 import Configure from '../Configure'; 
-import logo from '../assets/images/TTLogo.png';
+import Navbar from '../components/Navbar';
 import './App.css';
 
-
-function App() {
-    return (
-      <Router>
-        <div className="App">
-          <nav className="navbar">
-            <img src={logo} alt="Logo" className="logo" />
-            <div className="menu">
-              <Link to="/">Home</Link>
-              <Link to="/configure">Generate Podcast</Link>
-              {/* Add other links as needed */}
-            </div>
-          </nav>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/configure" element={<Configure />} />
-            {/* Add other routes as needed */}
-          </Routes>
-        </div>
-      </Router>
-    );
-}
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar /> {/* Include the Navbar component */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/configure" element={<Configure />} />
+          {/* Add other routes as needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
