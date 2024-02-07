08c23872218e57d3ab8cf7d98d0de274ac9091a0
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2024-02-06 22:49:59 -0500

    Refactor Navbar component to dynamically generate links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,13 @@ import logo from '../assets/logo.svg';
 
 const li_hover = "text-gray-900 dark:text-white hover:font-bold"
 
+const tabs = [["/", "Home"], ["/about", "About"], ["/contact", "Contact Us"]]
+const links = tabs.map(string => {
+    return <Link href={string[0]}>{string[1]}</Link>
+})
+
+
+
 const NavBar: React.FC = () => {
   return (
     <nav className="flex justify-between items-center py-4 px-6 bg-white dark:bg-gray-900">
@@ -16,17 +23,10 @@ const NavBar: React.FC = () => {
       </div>
 
       <ul className="flex space-x-4">
-        <li className={li_hover}>
-          <Link href="/"> Home </Link>
-        </li>
-        <li className={li_hover}>
-          <Link href="/about"> About Us </Link>
-        </li>
-        <li className={li_hover}>
-          <Link href="/contact">Contact Us</Link>
-        </li>
+        {links.map((link, index) => {
+            return <li key={index} className={li_hover}>{link}</li>
+        })}
       </ul>
-      {/* Update button classes for dark mode */}
       <button className="btn rounded-full px-6 py-2 bg-black text-white font-bold hover:bg-gray-800 border border-transparent dark:border-white dark:bg-white dark:text-black dark:hover:bg-gray-900 dark:hover:text-white transition duration-300">
         <Link href="/login">Create Your Podcast</Link>
       </button>
