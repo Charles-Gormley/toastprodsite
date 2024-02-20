ce8cd3a70061866166c85322c00df65a5afa794d
Joshua Meltzer <jlmeltz928@gmail.com>
2024-02-20 10:00:11 -0500

    fix useRouter import

--- src/app/verification/[email]/page.tsx
+++ src/app/verification/[email]/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import VerificationInput from "react-verification-input";
 
 export default function Verification({ params }: { params: { email: string}}) {
