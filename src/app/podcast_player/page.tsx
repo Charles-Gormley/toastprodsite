e149e249c5a2276124e26dbb3f845506bf119b23
Charlie <ceg98@drexel.edu>
2024-02-26 20:51:00 -0500

    functionality done.

--- src/app/podcast_player/page.tsx
+++ src/app/podcast_player/page.tsx
@@ -6,8 +6,10 @@ import "@madzadev/audio-player/dist/index.css";
 import { getCookie, setCookie } from "/src/components/cookies.tsx";
 import { LinkedList } from "/src/components/LinkedList.tsx";
 
+// TODO: Fix Next button, if you click it multiple times it will fail. 
 
-import React, { useEffect, useState } from 'react';
+
+import React, { useEffect, useState, useRef } from 'react';
 import { create } from "domain";
 import { parse } from "path";
 
@@ -35,7 +37,6 @@ let maxIndex = ceilIndex; // iniitalize maxIndex to the ceilIndex.
 let lastSegmentIndex = -1; // last segment index that was played.
 
 // TODO: We need Automatic Next Button
-// TODO: Call Content Preview.
 
 
 // Audio Player
@@ -46,7 +47,9 @@ interface AudioPlayerProps {
 const AudioPlayer: React.FC<AudioPlayerProps> = () => {
   const [audioSrc, setAudioSrc] = useState<string | null>(null);
   const [podcastIndex, setPodcastIndex] = useState('intro');
+  var [content_preview, setContentPreview] = useState<Record<string, string>  | null>(null);
   
+  const audioPlayerRef = useRef<HTMLAudioElement>(null);
 
   // Previous Podcast Index
   const previousPodcastIndex = () => {
@@ -56,7 +59,7 @@ const AudioPlayer: React.FC<AudioPlayerProps> = () => {
     }
   };
 
-  async function getSegment(podcastIndex: string) {
+  async function getPodcastPreview(podcastIndex: string): Promise<Record<string, string>> {
     const previewUrl = `${base}podcast-preview/`;
     const email = getCookie("email");
     const jwt = getCookie("jwt-token");
@@ -80,15 +83,20 @@ const AudioPlayer: React.FC<AudioPlayerProps> = () => {
         // Getting Presigned S3 URL
         const jsonResponse = await response.json();
         console.log('get segment response', jsonResponse);
-        
-        // TODO: To obtain the body of the response
+
+        const podcastPreview = jsonResponse.podcast_preview;
+        return podcastPreview; // {topic, content_preview_title, script, source}
       }
       else {
         console.error("Failed to load audio");
+        return {"Status": "Failed"};
+        // TODO: Add Error Handling
       }
     }
     catch {
       console.error("Error fetching audio");
+      return {"Status": "Failed"};
+      // TODO: Add Error Handling
     }
   }
 
@@ -203,12 +211,19 @@ const AudioPlayer: React.FC<AudioPlayerProps> = () => {
 
         const stream_url = jsonResponse.url;
         console.log('url', stream_url);
+        
+        if (podcastIndex !== 'intro') {
+          content_preview = await getPodcastPreview(podcastIndex);
+          setContentPreview(content_preview);
+        }
+
 
         setAudioSrc(stream_url);
       }
       else {
         console.error("Failed to load audio");
         // Display Error onto the screen.
+        // TODO: Add Error Handling
       }
     }
     catch { 
@@ -217,13 +232,27 @@ const AudioPlayer: React.FC<AudioPlayerProps> = () => {
       // Display Error onto the screen.
     }
   }
+
   useEffect(() => {
     streamAudio(podcastIndex);
+    const player = audioPlayerRef.current;
+    if (player) {
+      const handleAudioEnd = () => nextPodcastIndex();
+      player.addEventListener('ended', handleAudioEnd);
+
+      // Cleanup function to remove event listener
+      return () => player.removeEventListener('ended', handleAudioEnd);
+    }
   }, [podcastIndex]);
 
   return (
     <div>
-      <audio id="audioPlayer" controls src={audioSrc}></audio> // TODO: If its done, it'll be event. 
+      <audio id="audioPlayer" ref={audioPlayerRef} autoPlay controls src={audioSrc}></audio> // TODO: If its done, it'll be event. 
+      
+      <div>
+        <h1>{content_preview?.content_preview_title}</h1>
+        <p>{content_preview?.script}</p>
+      </div>
       <div>
         <button onClick={previousPodcastIndex}>Previous</button>
         <button onClick={nextPodcastIndex}>Next</button>
