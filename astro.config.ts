9350287c046e1b6386707926743425753d1e9080
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-03-05 09:20:40 -0500

    chore: a comment

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [
+    // Allows writing nested CSS declarations in Astro components.
     tailwind({ nesting: true })
   ],
   prefetch: { prefetchAll: true },
